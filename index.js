9181e9e5ac0a3fafca2e09499a855918abddc7a1
Raihan Mahmud <raihan_uits@outlook.com>
2022-05-25 09:22:17 +0600

    scripting modal

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 
-
+$(document).ready(function () {
+    $("#loginButton").click(function () {
+        $("#loginModal").modal("toggle");
+    })
+});
 document.getElementById('copyright').innerHTML = `<b>COPYRIGHT ${new Date().getFullYear()} © PRISM</b>`
