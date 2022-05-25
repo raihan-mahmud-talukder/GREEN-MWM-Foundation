139df54ba9c63bb1b6f947c1ac5341b2d0b6a721
Raihan Mahmud <raihan_uits@outlook.com>
2022-05-25 09:43:51 +0600

    login ad user/guest done

--- index.js
+++ index.js
@@ -4,4 +4,16 @@ $(document).ready(function () {
         $("#loginModal").modal("toggle");
     })
 });
+
+document.getElementById('submit').onclick = function (e) {
+    if (document.getElementById('visitor').value === 'user') {
+        location.href = 'user.html'
+    }
+    else {
+        location.href = 'guest.html'
+    }
+
+}
+
+
 document.getElementById('copyright').innerHTML = `<b>COPYRIGHT ${new Date().getFullYear()} © PRISM</b>`
